1a9f630e62830903b5df8b51d692476f3c0afd48
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-04-03 10:06:15 +0530

    update the scripts

--- eks-cluster/scripts.sh/ebs.sh
+++ eks-cluster/scripts.sh/ebs.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-package_installation "Verifying Node Role existence..."
+EBS_CSI_POLICY_ARN="arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+ECR_POLICY_ARN="arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
+
+echo  "Verifying Node Role existence..."
 NODE_ROLE=$(aws iam list-roles --query "Roles[?contains(RoleName, 'eksctl-eks-cli-prac-nodegroup-ng1-NodeInstanceRole')].RoleName" --output text)
 
 if [ -z "$NODE_ROLE" ] || [ "$NODE_ROLE" == "None" ]; then
-    print_message "No IAM role found matching the pattern. Exiting."
-    exit 1
+    echo  "No IAM role found matching the pattern. Exiting."
 else
     print_message "Node Role found: $NODE_ROLE. Attaching policy..."
-    aws iam attach-role-policy --role-name "$NODE_ROLE" --policy-arn "$EBS_CSI_POLICY_ARN" || { print_message "Failed to attach policy"; exit 1; }
-    aws iam attach-role-policy --role-name "$NODE_ROLE" --policy-arn "$ECR_ARN" || { print_message "Failed to attach policy"; exit 1; }
+    aws iam attach-role-policy --role-name "$NODE_ROLE" --policy-arn "$EBS_CSI_POLICY_ARN"
+    aws iam attach-role-policy --role-name "$NODE_ROLE" --policy-arn "$ECR_POLICY_ARN"
     print_message "Policys attached successfully."
 fi
